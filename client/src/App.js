a38467346c343268ed0bfeff3cd42296ba222a52
ShereenMessi <shereenmessi@hotmail.com>
2020-11-22 22:40:30 +0000

    updated header

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1>Fib Calculater updated</h1>
+                    <h1>Changed</h1>
                     <Link to="/">Home</Link>
                     <Link to="/otherpage">Other Page</Link>
                 </header>
