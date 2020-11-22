bed538b4779bbcaa22e93f1b8849ac0febd7595f
ShereenMessi <shereenmessi@hotmail.com>
2020-11-22 22:45:29 +0000

    updated header

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
     return (
         <div>
-            I am some other page!
+            I am some other page! changed!!!
             <Link to="/" >Go back home</Link>
         </div>
     );
